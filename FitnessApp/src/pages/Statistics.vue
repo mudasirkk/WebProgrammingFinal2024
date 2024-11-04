f15c28ff06e62710229049a8fe4aaba774a0f2df
mudasirkk <mudasirkkhan@gmail.com>
2024-11-04 14:03:34 -0500

    Update Statistics.vue

--- FitnessApp/src/pages/Statistics.vue
+++ FitnessApp/src/pages/Statistics.vue
@@ -1,15 +1,28 @@
+<script setup lang="ts">
+
+
+
+</script>
+
 <template>
-    <div class="statistics">
-      <h1>This is a Statistics page</h1>
+    <div class="container">
+      <article class="message is-link">
+        <div class="message-header">
+          <p>Workout Statistics</p>
+        </div>
+        <div class="message-body">
+          <h1>Stats</h1>
+        </div>
+      </article>
     </div>
-  </template>
-  
-  <style>
-  @media (min-width: 1024px) {
-    .about {
-      min-height: 100vh;
-      display: flex;
-      align-items: center;
-    }
-  }
-  </style>
+</template>
+
+<style scoped>
+.container{
+  padding: 10rem;  
+}
+.message-header{
+  background-color: orange;
+  align-self: auto;
+}
+</style>
